82cbd517809ebf80e23c30e606aaca97e2508f34
Camille Appert <camille@appert.me>
2020-01-14 01:09:07 +0100

    https & www everywhere

--- config.rb
+++ config.rb
@@ -30,7 +30,7 @@ page '/sitemap.xml', :layout => false, :format => :xhtml
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
+# Proxy pages (https://middlemanapp.com/advanced/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
